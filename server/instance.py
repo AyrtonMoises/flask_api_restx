7e0e7a2ed0a584f8e4c84e8da04286a085836f17
ayrtonmoises <ayrton.sousa@gmail.com>
2022-04-29 13:29:04 -0300

    first commit

---
+++ server/instance.py
@@ -0,0 +1,40 @@
+from flask import Flask, Blueprint
+from flask_restx import Api
+
+class Server():
+
+    def __init__(self,):
+        self.app = Flask(__name__)
+        self.blueprint = Blueprint('api', __name__, url_prefix='/api')
+        self.api = Api(
+            self.blueprint,
+            version='1.0',
+            title="Sample Book API",
+            description='A simples book API',
+            doc='/doc'
+        )
+        self.app.register_blueprint(self.blueprint)
+
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+        self.app.config['PROPAGATE_EXCEPTIONS'] = True
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+        self.book_ns = self.book_ns()
+
+    def book_ns(self,):
+        return self.api.namespace(
+            name='Books',
+            description='books related operations',
+            path='/'
+        )
+
+
+    def run(self,):
+        self.app.run(
+            port=5000,
+            host='0.0.0.0',
+            debug=True
+        )
+
+    
+server = Server()
